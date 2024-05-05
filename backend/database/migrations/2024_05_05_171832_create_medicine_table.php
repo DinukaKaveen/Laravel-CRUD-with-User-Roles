43d612c13f6b3c9bb20c03416a935db4dfae960d
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-05-06 00:01:30 +0530

    Update 2024_05_05_171832_create_medicine_table.php

--- backend/database/migrations/2024_05_05_171832_create_medicine_table.php
+++ backend/database/migrations/2024_05_05_171832_create_medicine_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('price');
-            $table->string('quantity');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
