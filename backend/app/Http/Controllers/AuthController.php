ce5cd56815bdafe851e8e3021c08b5ae49f7028c
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-05-05 08:55:02 +0530

    Update AuthController.php

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -65,4 +65,11 @@ class AuthController extends Controller
         }
 
     }
+
+    public function logout(Request $request)
+    {
+        // Revoke the current user's token
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
 }
