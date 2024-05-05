f820d73526c31294848fdd553fdcaef9e69270e9
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-05-05 10:03:48 +0530

    Update ManagerMiddleware.php

--- backend/app/Http/Middleware/ManagerMiddleware.php
+++ backend/app/Http/Middleware/ManagerMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class ManagerMiddleware
@@ -15,7 +16,7 @@ class ManagerMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->user()->role !== "Manager") {
+        if (Auth::user()->role !== "Manager") {
             return response()->json(['error' => 'Unauthorized'], 403);
         }
         return $next($request);
